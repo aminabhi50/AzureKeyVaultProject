6fb8958d2d78a533bfb459c3798003913904e9b6
aminabhi50 <aminabhi50@gmail.com>
2024-11-12 05:50:42 +0000

    Updated

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -1,22 +1,57 @@
 using Azure.Security.KeyVault.Secrets;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Certificates;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Threading.Tasks;
 
 public class IndexModel : PageModel
 {
     private readonly SecretClient _secretClient;
+    private readonly KeyClient _keyClient;
+    private readonly CertificateClient _certificateClient;
 
     public string SecretValue { get; private set; }
+    public string KeyName { get; private set; }
+    public string KeyType { get; private set; }
+    public int? KeySize { get; private set; }
+    public string CertificateName { get; private set; }
+    public string CertificateValue { get; private set; }
+    public string CertificateIssuer { get; private set; }
+    public string CertificateSubject { get; private set; }
 
-    public IndexModel(SecretClient secretClient)
+    public IndexModel(SecretClient secretClient, KeyClient keyClient, CertificateClient certificateClient)
     {
         _secretClient = secretClient;
+        _keyClient = keyClient;
+        _certificateClient = certificateClient;
     }
 
     public async Task OnGetAsync()
     {
+        // Retrieve a secret
         var secretName = "connection-string";
         KeyVaultSecret secret = await _secretClient.GetSecretAsync(secretName);
         SecretValue = secret.Value;
+
+        // Retrieve a key
+        var keyName = "encryption-key";
+        KeyVaultKey key = await _keyClient.GetKeyAsync(keyName);
+        KeyName = key.Name;
+        KeyType = key.KeyType.ToString();
+        KeySize = key.Key.N?.Length * 8; // Key size in bits (for RSA keys)
+
+        // Retrieve a certificate
+        var certificateName = "ssl-certificate";
+        var certificate = await _certificateClient.GetCertificateAsync(certificateName);
+        CertificateName = certificate.Value.Name;
+
+        // Retrieve issuer and subject from the certificate policy
+        CertificateIssuer = certificate.Value.Policy.IssuerName; // Correct way to get the issuer name
+        CertificateSubject = certificate.Value.Policy.Subject;   // Correct way to get the subject name
+
+        // Retrieve the certificate's raw value from Key Vault as a secret
+        var certificateSecret = await _secretClient.GetSecretAsync(certificateName);
+        CertificateValue = certificateSecret.Value.ToString(); // This is the base64-encoded certificate data
+
     }
 }
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,5 +1,7 @@
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Certificates;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,9 +14,16 @@ builder.Services.AddRazorPages();
 // Access the Key Vault URL from appsettings.json
 var keyVaultUrl = builder.Configuration["AzureKeyVault:VaultUrl"];
 
-// Configure Azure Key Vault client
-var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
-builder.Services.AddSingleton(client);
+// Configure Azure Key Vault clients for secrets, keys, and certificates
+var credential = new DefaultAzureCredential();
+var secretClient = new SecretClient(new Uri(keyVaultUrl), credential);
+var keyClient = new KeyClient(new Uri(keyVaultUrl), credential);
+var certificateClient = new CertificateClient(new Uri(keyVaultUrl), credential);
+
+// Register each client as a singleton service
+builder.Services.AddSingleton(secretClient);
+builder.Services.AddSingleton(keyClient);
+builder.Services.AddSingleton(certificateClient);
 
 var app = builder.Build();
 
